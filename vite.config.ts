10589d1ac68a35c1eb56c3222945fa6831d85f8a
Katame <h.k.hiro.2117@gmail.com>
2025-05-20 21:47:33 +0900

    テスト用にconfig変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "react-study-todo",
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true, // expect(), descrive()/ it()を毎回インポートしなくてよくなる
+    environment: "jsdom", // ブラウザのようなDOMを仮想で用意
+    include: ["tests/**/*.test.{ts,tsx}"], // テスト対象のファイルパターン
+    setupFiles: ["./tests/setup.ts"], // jest-domの拡張マッチャーを有効化するファイル
+  },
+  base: "/react-study-todo",
 });
